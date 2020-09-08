e6ae30669b8044a594c53e77f98815b03d80c1fc
imhilla <hillaryodhiambo282@gmail.com>
2020-09-08 14:03:10 +0300

    add on press function

--- script.js
+++ script.js
@@ -2,36 +2,63 @@
 //   document.getElementById("para").style.display = "block"
 // })
 
-
-const players =()=> {
+const players = () => {
   const firstPlayerName = prompt("Please Enter First Player Name?");
   const secondPlayerName = prompt("Please Enter Second Player Name?");
   const firstPlayerSymbol = "X";
   const secondPlayerSymbol = "O";
   let playerTurn = 0;
+  const turn = () => {
+    if (playerTurn == 0) {
+      playerTurn += 1;
+      return [firstPlayerName, firstPlayerSymbol];
+    } else {
+      playerTurn -= 1;
+      return [secondPlayerName, secondPlayerSymbol];
+    }
+  };
   return {
-    firstPlayerName, secondPlayerName, firstPlayerSymbol, secondPlayerSymbol
-  }
+    firstPlayerName,
+    secondPlayerName,
+    turn,
+  };
 };
 
 let newGame = players();
 
-const board = ((index, symbol) => {
-  let board_array = ["X", "", "", "X", "X", "O", "X", "X", "X"];
+const board = (() => {
+  let board_array = ["", "", "", "", "", "", "", "", ""];
   let display_board = () => {
-    board_array.forEach(function (items, index){
+    board_array.forEach(function (items, index) {
       document.getElementById(`inner-board-${index}`).textContent = `${items}`;
     });
   };
-  return { display_board };
-
+  let updateBoard = (index, symbol) => {
+    if (board_array[index] == "") {
+      board_array[index] = symbol;
+      return true;
+    } else {
+      return false;
+    }
+  };
+  return { display_board, updateBoard, board_array };
 })();
 
-
 let myBoard = board;
-let game = true;
-while (game == true){
+let checked = true;
+const onPress = (id) => {
+  let index = parseInt(id[id.length - 1]);
+  if (checked == true) {
+    console.log("yes");
+  } else {
+    console.log("no");
+  }
+  let playersTurn = newGame.turn();
+  checked = myBoard.updateBoard(index, playersTurn[1]);
   myBoard.display_board();
 };
 
-
+// let game = true;
+// while (game == true) {
+//   myBoard.display_board();
+// }
