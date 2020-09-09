97f4b5ca34cf059db96b8c6cfc1b1a55011e7a9e
Muhammad Fahad Umer <eng.fahadumer@gmail.com>
2020-09-09 22:34:25 +0500

    Add Required Changes

--- script.js
+++ script.js
@@ -1,41 +1,8 @@
-let newGame;
-
-const playersFactory = () => {
-  let player = { firstPlayerName: "", secondPlayerName: "",firstPlayerSymbol: "X", secondPlayerSymbol: "O"  };
-
-  player.firstPlayerName = document.getElementById("name1").value;
-  player.secondPlayerName = document.getElementById("name2").value;
-
-  document.getElementById(
-    "playersturn"
-  ).textContent = `${player.firstPlayerName}'s turn`;
-
-  let playerTurn = 0;
-  const turn = () => {
-    if (playerTurn === 0) {
-      playerTurn += 1;
-      document.getElementById(
-        "playersturn"
-      ).textContent = `${player.secondPlayerName}'s turn`;
-      return [player.firstPlayerName, player.firstPlayerSymbol];
-    }
-    playerTurn -= 1;
-    document.getElementById(
-      "playersturn"
-    ).textContent = `${player.firstPlayerName}'s turn`;
-    return [player.secondPlayerName, player.secondPlayerSymbol];
-  };
-  return { turn };
-};
-
-document.getElementById("playgame").addEventListener("click", () => {
-  document.getElementById("outer-board").style.display = "grid";
-  document.getElementById("playgame").style.display = "none";
-  newGame = playersFactory();
-});
+const player = (name, symbol) => ({ name, symbol });
 
 const board = (() => {
-  const boardArray = ["", "", "", "", "", "", "", "", ""];
+  let winCheck = true;
+  const boardArray = ['', '', '', '', '', '', '', '', ''];
   const winArray = [
     [1, 2, 3],
     [4, 5, 6],
@@ -46,75 +13,86 @@ const board = (() => {
     [3, 5, 7],
     [1, 5, 9],
   ];
+
+  const updateBoard = (index, player) => {
+    if (winCheck === true) {
+      boardArray[index] = player.symbol;
+    }
+  };
+
   const displayBoard = () => {
     boardArray.forEach((items, index) => {
       document.getElementById(`inner-board-${index}`).textContent = `${items}`;
     });
   };
 
-  const updateBoard = (index, symbol) => {
-    if (boardArray[index] === "") {
-      boardArray[index] = symbol;
-    }
-  };
-  const winStatus = (moves) => {
+  const winStatus = (player) => {
     winArray.forEach((item) => {
       let count = 0;
       item.forEach((char) => {
-        if (boardArray[char - 1] === moves[1]) {
+        if (boardArray[char - 1] === player.symbol) {
           count += 1;
         }
         if (count === 3) {
-          setTimeout(() => {
-            document.querySelector("#outer-board").style.display = "none";
-            document.querySelector("#playersturn").style.display = "none";
-            document.querySelector("#winstatus").style.display = "block";
-            document.getElementById(
-              "winstatus"
-            ).textContent = `Congratulations ${moves[0]}! You Win `;
-            document.getElementById("reloadbutton").style.display = "block";
-          }, 100);
+          winCheck = false;
+          document.getElementById('winstatus').textContent = `Congratulations ${player.name}! You Win `;
         }
       });
     });
   };
 
   const drawStatus = () => {
-    if (!boardArray.includes("")) {
-      setTimeout(() => {
-        document.querySelector("#outer-board").style.display = "none";
-        document.querySelector("#playersturn").style.display = "none";
-        document.querySelector("#winstatus").style.display = "block";
-        document.querySelector("#winstatus").textContent =
-          "OHH NOO! Game Draw ";
-        document.getElementById("reloadbutton").style.display = "block";
-      }, 100);
+    if (winCheck === true && !boardArray.includes('')) {
+      document.getElementById('winstatus').textContent = 'Game Draw! Try Again.';
     }
   };
-
   return {
-    displayBoard,
-    updateBoard,
-    boardArray,
-    winStatus,
-    drawStatus,
+    boardArray, updateBoard, displayBoard, winStatus, drawStatus,
   };
 })();
 
-const myBoard = board;
-const onPress = (id) => {
-  // eslint-disable-line no-unused-vars
-  const index = parseInt(id[id.length - 1], 10);
-  if (myBoard.boardArray[index] === "") {
-    const playersTurn = newGame.turn();
-    myBoard.updateBoard(index, playersTurn[1]);
-    myBoard.displayBoard();
-    myBoard.drawStatus();
-    myBoard.winStatus(playersTurn);
-  }
+const playGame = () => {
+  let firstPlayerName;
+  let secondPlayerName;
+  let playerOne;
+  let playerTwo;
+  let currentPlayer;
+  let count = 0;
+  const switchPlayer = () => {
+    if (count === 1) {
+      count -= 1;
+      currentPlayer = playerTwo;
+    } else {
+      count += 1;
+      currentPlayer = playerOne;
+    }
+  };
+
+  document.getElementById('playgame').addEventListener('click', () => {
+    firstPlayerName = document.getElementById('name1').value;
+    secondPlayerName = document.getElementById('name2').value;
+    document.getElementById('outer-board').style.display = 'grid';
+    playerOne = player(firstPlayerName, 'X');
+    playerTwo = player(secondPlayerName, 'O');
+    document.getElementById('playgame').style.display = 'none';
+    document.getElementById('reloadbutton').style.display = 'block';
+  });
+
+  document.getElementById('reloadbutton').addEventListener('click', () => {
+    window.location.reload();
+  });
+
+  const play = (event) => {
+    const index = event.target.getAttribute('data-index');
+    if (board.boardArray[index] === '') {
+      switchPlayer();
+      board.updateBoard(index, currentPlayer);
+      board.displayBoard();
+      board.winStatus(currentPlayer);
+      board.drawStatus();
+    }
+  };
+  document.getElementById('outer-board').addEventListener('click', play);
 };
 
-function playAgain() {
-  // eslint-disable-line no-unused-vars
-  window.location.reload();
-}
+playGame();
